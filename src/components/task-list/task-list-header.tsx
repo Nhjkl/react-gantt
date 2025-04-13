6fbd3a7df6722b535a65d3224882a57547b84a2f
liuxian <liuxiangogo@gmail.com>
2025-04-13 13:19:18 +0800

    update

--- src/components/task-list/task-list-header.tsx
+++ src/components/task-list/task-list-header.tsx
@@ -6,7 +6,8 @@ export const TaskListHeaderDefault: React.FC<{
   rowWidth: string;
   fontFamily: string;
   fontSize: string;
-}> = ({ headerHeight, fontFamily, fontSize, rowWidth }) => {
+  locale: string;
+}> = ({ headerHeight, fontFamily, fontSize, rowWidth, locale }) => {
   return (
     <div
       className={styles.ganttTable}
@@ -27,37 +28,37 @@ export const TaskListHeaderDefault: React.FC<{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;Name
+          {locale === "zh" ? "任务名称" : "Name"}
         </div>
-        <div
-          className={styles.ganttTable_HeaderSeparator}
-          style={{
-            height: headerHeight * 0.5,
-            marginTop: headerHeight * 0.2,
-          }}
-        />
+        {/* <div */}
+        {/*   className={styles.ganttTable_HeaderSeparator} */}
+        {/*   style={{ */}
+        {/*     height: headerHeight * 0.5, */}
+        {/*     marginTop: headerHeight * 0.2, */}
+        {/*   }} */}
+        {/* /> */}
         <div
           className={styles.ganttTable_HeaderItem}
           style={{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;From
+          {locale === "zh" ? "开始时间" : "From"}
         </div>
-        <div
-          className={styles.ganttTable_HeaderSeparator}
-          style={{
-            height: headerHeight * 0.5,
-            marginTop: headerHeight * 0.25,
-          }}
-        />
+        {/* <div */}
+        {/*   className={styles.ganttTable_HeaderSeparator} */}
+        {/*   style={{ */}
+        {/*     height: headerHeight * 0.5, */}
+        {/*     marginTop: headerHeight * 0.25, */}
+        {/*   }} */}
+        {/* /> */}
         <div
           className={styles.ganttTable_HeaderItem}
           style={{
             minWidth: rowWidth,
           }}
         >
-          &nbsp;To
+          {locale === "zh" ? "结束时间" : "To"}
         </div>
       </div>
     </div>
